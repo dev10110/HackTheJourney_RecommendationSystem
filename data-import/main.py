3536252b0bb86ea72aa3dd136557160dc676e8fa
Vishnu R Menon <vishthemenon@gmail.com>
2019-07-27 20:25:34 +0100

    Refactor api class into AmadeusClient class

--- data-import/main.py
+++ data-import/main.py
@@ -1,39 +1,9 @@
-import requests
-import json
 import pickle
-import os
 
-base_url = 'https://api.amadeus.com/v1/'
+from AmadeusClient import AmadeusClient
 
-auth_endpoint = 'security/oauth2/token'
-auth_url = base_url + auth_endpoint
-data = {
-	'grant_type': 'client_credentials',
-	'client_id': os.environ.get('CLIENT_ID'),
-	'client_secret': os.environ.get('CLIENT_SECRET') 
-}
-headers = {
-	'content-type': 'application/x-www-form-urlencoded'
-}
-r = requests.post(auth_url, data=data, headers=headers)
-j = json.loads(r.text)
-token = j['access_token']
+am = AmadeusClient()
 
+data = am.get_poi(lat=51.50, lon=-0.177)
 
-
-
-poi_endpoint = 'reference-data/locations/pois'
-poi_url = base_url + poi_endpoint
-params = {
-	'latitude': 51.50,
-	'longitude': -0.177,
-	'page[limit]': 1000,
-	'r': 20
-}
-headers = {
-	'Authorization': 'Bearer ' + token
-}
-
-r = requests.get(poi_url, headers=headers, params=params)
-j = json.loads(r.text)['data']
-pickle.dump(j, open('london_poi.pickle', 'wb'))
+pickle.dump(data, open('london_poi.pickle', 'wb'))
