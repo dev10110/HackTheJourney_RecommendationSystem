3ee6ddf0a116d28444a0905e3378f35b05335642
CatSaidMeow <julianbrendel99@gmail.com>
2019-07-28 13:16:26 +0100

    added POI endpoint

--- recommender/app.py
+++ recommender/app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, request, abort
 from RecommenderClient import RecommenderClient
 import pickle
 import json
-
+import random
 
 app = Flask(__name__)
 data = pickle.load(open("london_poi.pickle", "rb"))
@@ -14,10 +14,20 @@ def index():
     return "Call /getRecommendations"
 
 
+@app.route("/getPOI")
+def getPOI():
+
+    limit = request.args.get("limit", 10)
+
+    choices = random.choices(data, k=limit)
+
+    return jsonify(choices)
+
+
 @app.route("/getRecommendations")
 def getRecommendations():
-
     likes = request.args.get("likes")
+    limit = request.args.get("limit", 10)
 
     if not likes:
         abort(400, "Missing parameter likes")
@@ -31,7 +41,7 @@ def getRecommendations():
 
     out = list()
 
-    for entry in client.suggest(likes=likes):
+    for entry in client.suggest(k=limit, likes=likes):
         entry["data"] = data[entry.get("index")]
         out.append(entry)
 
