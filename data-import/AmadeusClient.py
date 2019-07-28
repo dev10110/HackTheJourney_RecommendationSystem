02e25bbf70b2b3b9a4d020808dec5d744665185b
Dev <dra16@ic.ac.uk>
2019-07-28 13:16:55 +0100

    updatedAmClient

--- data-import/AmadeusClient.py
+++ data-import/AmadeusClient.py
@@ -2,6 +2,7 @@ import requests
 
 import json
 import os
+import pandas as pd
 
 class AmadeusClient:
 	base_url = 'https://api.amadeus.com/v1/'
@@ -17,7 +18,7 @@ class AmadeusClient:
 		auth_data = {
 			'grant_type': 'client_credentials',
 			'client_id': os.environ.get('CLIENT_ID'),
-			'client_secret': os.environ.get('CLIENT_SECRET') 
+			'client_secret': os.environ.get('CLIENT_SECRET')
 		}
 		auth_headers = {
 			'content-type': 'application/x-www-form-urlencoded'
@@ -28,7 +29,8 @@ class AmadeusClient:
 
 
 
-	def get_poi(self, lat, lon, rad=20, lim=1000):
+	def get_poi(self, lat, lon, rad=20, lim=1000, name=''):
+
 		poi_endpoint = 'reference-data/locations/pois'
 		poi_url = self.base_url + poi_endpoint
 		poi_params = {
@@ -42,4 +44,24 @@ class AmadeusClient:
 		}
 
 		r = requests.get(poi_url, headers=poi_headers, params=poi_params)
-		return json.loads(r.text)['data']
+
+		data = json.loads(r.text)['data']
+
+		# add in the city name
+		for el in data:
+			el['city'] = name
+
+
+		return data
+
+	build_default_data(self, kpercity = 20):
+		#builds the data set for the default list of cities
+		cities = pd.read_csv('defaultCities.csv')
+
+		data = []
+		for i in range(len(cities)):
+		    citydata = self.get_poi(lat=cities['Latitude'][i], lon=cities['Longitude'][i], name=cities['Name'][i],lim=kpercity)
+		    print('Loaded city: %s'%cities['Name'][i])
+		    data += citydata
+
+		return data
--- data-import/main.py
+++ data-import/main.py
@@ -1,8 +1,14 @@
 import pickle
+import pandas
 
 from AmadeusClient import AmadeusClient
 
 am = AmadeusClient()
+
+cities = pd.DataFrame.from_csv('defaultCities.csv')
+
+cities['london']
+]
 
 #data = am.get_poi(lat=51.50, lon=-0.177)
 data = am.get_poi(lat=41.39715, lon=2.160873)
